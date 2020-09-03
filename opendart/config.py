342a5222ebca73704a084a614ef6743504a415e1
Hong SeungPyo <spkbk98@gmail.com>
2020-09-03 14:07:01 +0000

    implemented corpcode module

---
+++ opendart/config.py
@@ -0,0 +1,7 @@
+import os
+
+crtfc_key = '2f721ab8d5583923399e3b459b2fc92fcc358152'
+dataPath = 'data'
+corpCodeXmlPath = os.path.join(dataPath, 'CORPCODE.xml')
+corpCodePklPath = os.path.join(dataPath, 'corpCode.pkl')
+corpCodeZipPath = os.path.join(dataPath, 'corpCode.zip')
